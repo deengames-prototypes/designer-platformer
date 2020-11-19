05acf2401a15522e74810973e5a7d9c61fe8e981
nightblade9 <nightbladecodes@gmail.com>
2020-11-18 21:58:03 -0500

    Fixed bugs by using base level scene. This makes level switching easier. Reloading on death will be easy, too.

--- prototype/BaseLevel.gd
+++ prototype/BaseLevel.gd
@@ -6,24 +6,20 @@ onready var _victory = $Victory/Control
 
 var _game_over:bool = false
 
-func load_level(number:int) -> void:
-	var level_type = load("res://Levels/Level" + str(number) + ".tscn")
-	var level = level_type.instance()
-	
-	for child in level.get_children():
+func _ready() -> void:
+	for child in get_children():
 		if child is Exit:
 			child.connect("player_exited", self, "_on_player_exited")
 		# TODO: find player, note location, and respawn there if dies
 			
-	add_child(level)
-
 func _on_player_exited(player:KinematicBody2D) -> void:
 	# prevent it from happening multiple times
 	if player != null and is_instance_valid(player):
 		player.queue_free()
+		
 	_victory.visible = true
 	_game_over = true
 
 func _unhandled_input(_event) -> void:
 	if _game_over and Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://LevelSelection.tscn")
+		assert(get_tree().change_scene("res://LevelSelection.tscn") == OK)
--- prototype/LevelSelection.gd
+++ prototype/LevelSelection.gd
@@ -1,9 +1,4 @@
 extends Node2D
 
-const CoreGame = preload("res://CoreGame.tscn")
-
 func _on_Level1_pressed():
-	var game = CoreGame.instance()
-	game.load_level(1)
-	
-	get_tree().root.add_child(game)
+	assert(get_tree().change_scene("res://Levels/Level1.tscn") == OK)
